5723e209805d26013c86fff5a2777a958087627a
ian <clarksware24@gmail.com>
2018-11-12 19:32:11 -0700

    completed tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_notes#note'
+
+  get '/about', to: 'static_notes#about'
+
+  resources :notes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
